89f7506379e5b677320806bbd3d944ab6fa30241
mateus gotardi <mateus.gotardi@gmail.com>
2023-10-31 15:45:07 -0300

    walk system

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothSpeed = 0.3f;
+    public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
